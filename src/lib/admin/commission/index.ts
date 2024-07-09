66da099a01890b798fc1a4151bdb1a0b2b56bf68
unknown <jharleymark98@gmail.com>
2024-07-09 19:18:46 +0800

    9.7.24

--- src/lib/admin/commission/index.ts
+++ src/lib/admin/commission/index.ts
@@ -18,6 +18,39 @@ const calculateTotalSales = (user_id: string, sales: AllSales[]) => {
   }
   return totalSales;
 };
+
+function calculateDownlineCommission(
+  agent: Agent,
+  sales: AllSales[],
+  baseCommission: number = 0,
+  parentComm: number = 0
+): number {
+  const _func = agent.downline.reduce((sum, downline) => {
+    const downlineRate = baseCommission - downline.commissionRate;
+    let childComm = 0;
+    // let downlineRate = 0;
+
+    if (downline.downline.length !== 0) {
+      childComm = calculateDownlineCommission(
+        downline,
+        sales,
+        baseCommission,
+        downline.commissionRate
+      );
+    }
+    const pivot =
+      downlineRate * calculateTotalSales(downline.id, sales) + childComm;
+    // console.log(
+    //   `Downline rate: ${agent.tier} & ${downline.tier}:`,
+    //   calculateTotalSales(downline.id, sales),
+    //   downlineRate * calculateTotalSales(downline.id, sales),
+    //   calculateDownlineCommission(downline, sales, baseCommission)
+    // );
+    return (sum += pivot);
+  }, 0);
+  console.log(`Tier${agent.tier} :`, _func);
+  return _func;
+}
 export function calculateCommission(
   agent: Agent,
   sales: AllSales[],
@@ -25,13 +58,41 @@ export function calculateCommission(
 ): number {
   let directCommission =
     agent.commissionRate * calculateTotalSales(agent.id, sales);
-
-  let downlineCommission = agent.downline.reduce((acc, downlineAgent) => {
-    let downlineRate = (agent.commissionRate - downlineAgent.commissionRate)*10;
-    console.log("allocated", downlineRate);
-    console.log( 'this', calculateCommission(downlineAgent, sales), downlineRate * calculateCommission(downlineAgent, sales))
-    return acc + downlineRate* calculateCommission(downlineAgent, sales, baseCommission);
-  }, 0);
-  console.log(`Summary ${agent.tier}`, directCommission, downlineCommission);
+  // console.log(`Direct_comm ${agent.tier} :`, directCommission);
+  // let downlineCommission = agent.downline.reduce((acc, downlineAgent) => {
+  //   let downlineRate = baseCommission - downlineAgent.commissionRate;
+  //   console.log(
+  //     `This-tier${agent.tier} from ${downlineAgent.tier}:`,
+  //     calculateCommission(downlineAgent, sales, downlineAgent.commissionRate)
+  //   );
+  //   return (acc += calculateCommission(
+  //     downlineAgent,
+  //     sales,
+  //     downlineAgent.commissionRate
+  //   ));
+  // }, 0);
+  let downlineCommission = calculateDownlineCommission(
+    agent,
+    sales,
+    agent.commissionRate
+  );
   return directCommission + downlineCommission;
 }
+
+// export function calculateCommission(
+//   agent: Agent,
+//   sales: AllSales[],
+//   baseCommission: number = 0
+// ): number {
+//   let directCommission =
+//     agent.commissionRate * calculateTotalSales(agent.id, sales);
+
+//   let downlineCommission = agent.downline.reduce((acc, downlineAgent) => {
+//     let downlineRate = (agent.commissionRate - downlineAgent.commissionRate)*10;
+//     console.log("allocated", downlineRate);
+//     console.log( 'this', calculateCommission(downlineAgent, sales), downlineRate * calculateCommission(downlineAgent, sales))
+//     return acc + downlineRate* calculateCommission(downlineAgent, sales, baseCommission);
+//   }, 0);
+//   console.log(`Summary ${agent.tier}`, directCommission, downlineCommission);
+//   return directCommission + downlineCommission;
+// }
